e2729b738b157e0c569482fdbdfbe20fc9c46c79
DWONeill18 <d.w.oneill.18@gmail.com>
2019-06-14 15:51:02 +0100

    read functionality update v1.2

--- src/main/webapp/readScript.js
+++ src/main/webapp/readScript.js
@@ -1,32 +1,8 @@
-function fetchData(method, textbody, url) {
-    return new Promise((resolve, reject) => {
-        const req = new XMLHttpRequest();
-        req.onreadystatechange = () => {
-
-            if (req.readyState === 4) {
-
-                if (req.status >= 200 && req.status < 300) {           
-                    resolve(req.responseText);                  
-                }
-                else {
-                    reject("something went wrong");
-                }
-            }
-        };
-        req.open(method, "http://35.242.174.17:8080/ChessDatabaseAPI-1.0/api/" + url);
-        req.setRequestHeader("content-type", "application/json");
-        req.send(textbody);
-    });
-}
-
-console.log('before promise');
-
 let gameData;
 
-function getGame(){
-fetchData("GET", document.getElementById=("Game_id").value, "games").then((value) => {
+function getGame(id){
+fetchData("GET", null, "games/" + id).then((value) => {
  gameData=value}).catch((err) => {
     console.log('rejected. reason:', err)});
 console.log('after promise');
 }
-
---
+++ src/main/webapp/serverRequest.js
@@ -0,0 +1,25 @@
+function fetchData(method, textbody, url) {
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.onreadystatechange = () => {
+
+            if (req.readyState === 4) {
+
+                if (req.status >= 200 && req.status < 300) {           
+                    resolve(req.responseText);                  
+                }
+                else {
+                    reject("something went wrong");
+                }
+            }
+        };
+        req.open(method, "http://35.242.174.17:8080/ChessDatabaseAPI-1.0/api/" + url);
+        req.setRequestHeader("content-type", "application/json");
+        req.send(textbody);
+    });
+}
+
+console.log('before promise');
+
+
+
