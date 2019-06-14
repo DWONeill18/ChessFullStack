4496ec5ea2cd15f2ec13d2f4d64bb65cc2777f97
DWONeill18 <d.w.oneill.18@gmail.com>
2019-06-14 23:23:04 +0100

    deleteScript.js added and delete.html update

--- src/main/webapp/deleteScript.js
+++ src/main/webapp/deleteScript.js
@@ -1,1 +1,17 @@
-deleteScript;
+function deleteGame(){
+
+let id = document.getElementById("Game_id").value; 
+
+fetchData("DELETE", null, "games/" + id).then((value) => {
+    info = JSON.parse(value)
+    document.getElementById("Game_id").value = info.game_id;
+    document.getElementById("White").value = info.white;
+    document.getElementById("Black").value = info.black;
+    document.getElementById("Result").value = info.result;
+    document.getElementById("Eco").value = info.eco;
+    document.getElementById("Notation").value = info.notation;     
+}).catch((err) => {
+       console.log('rejected. reason:', err)});
+   console.log('after promise');
+
+}
