88029052c90745df245ef4c1109d26732df9bb7d
DWONeill18 <d.w.oneill.18@gmail.com>
2019-06-14 18:40:56 +0100

    read functional complete, output to boxes

--- src/main/webapp/readScript.js
+++ src/main/webapp/readScript.js
@@ -9,7 +9,9 @@ fetchData("GET", null, "games/" + id).then((value) => {
     document.getElementById("Game_id").value = info.game_id;
     document.getElementById("White").value = info.white;
     document.getElementById("Black").value = info.black;
-    document.getElementById("Result").value = info.result;  
+    document.getElementById("Result").value = info.result;
+    document.getElementById("Eco").value = info.eco;
+    document.getElementById("notation").value = info.notation;     
 }).catch((err) => {
        console.log('rejected. reason:', err)});
    console.log('after promise');
