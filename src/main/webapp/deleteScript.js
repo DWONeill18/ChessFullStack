f9be5d30fb9e3e85621e2b1f97cce91d1acf7a52
DWONeill18 <d.w.oneill.18@gmail.com>
2019-06-14 23:53:28 +0100

    deleteScript.js added and delete.html update

--- src/main/webapp/deleteScript.js
+++ src/main/webapp/deleteScript.js
@@ -3,15 +3,10 @@ function deleteGame(){
 let id = document.getElementById("Game_id").value; 
 
 fetchData("DELETE", null, "games/" + id).then((value) => {
-    info = JSON.parse(value)
-    document.getElementById("Game_id").value = info.game_id;
-    document.getElementById("White").value = info.white;
-    document.getElementById("Black").value = info.black;
-    document.getElementById("Result").value = info.result;
-    document.getElementById("Eco").value = info.eco;
-    document.getElementById("Notation").value = info.notation;     
-}).catch((err) => {
+    info = JSON.parse(value)}).catch((err) => {
        console.log('rejected. reason:', err)});
    console.log('after promise');
+
+   console.log("ID deleted")
 
 }
