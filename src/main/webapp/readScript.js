177b991f3aba307a02f62631ab2e362a157962c2
DWONeill18 <d.w.oneill.18@gmail.com>
2019-06-14 16:23:36 +0100

    read functionality update v1.4

--- src/main/webapp/readScript.js
+++ src/main/webapp/readScript.js
@@ -5,7 +5,13 @@ function getGame(){
 let id = document.getElementById("Game_id").value; 
 
 fetchData("GET", null, "games/" + id).then((value) => {
- gameData=value}).catch((err) => {
+ let gameData = JSON.parse(value.responseText);}).catch((err) => {
     console.log('rejected. reason:', err)});
 console.log('after promise');
+
+document.getElementById("Game_id").innerText = value;
+document.getElementById("White").innerText = value;
+document.getElementById("Black").innerText = value;
+document.getElementById("Result").innerText = value;
+
 }
