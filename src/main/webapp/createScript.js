15a627d733426ba1883802a8de7c61d26d3f62aa
DWONeill18 <d.w.oneill.18@gmail.com>
2019-06-14 11:22:24 +0100

    added javascript create functionality

--- src/main/webapp/createScript.js
+++ src/main/webapp/createScript.js
@@ -5,4 +5,12 @@ function submitCreate(){
     body.result = document.getElementById("Result").value;
     body.eco = document.getElementById("Eco").value;
     body.notation = document.getElementById("Notation").value;
+
+    let json = JSON.stringify(body);
+
+    let request = new XMLHttpRequest();
+    request.open("POST", "api/games/");
+    request.setRequestHeader("content-type", "application/json");
+    request.send(json);
+
 }
