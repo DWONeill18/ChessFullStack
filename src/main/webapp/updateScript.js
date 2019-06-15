157b0372de4471641d21c7720fd0bd24a9746b52
DWONeill18 <d.w.oneill.18@gmail.com>
2019-06-15 21:20:27 +0100

    corrected update functionality to include null values in input boxes

--- src/main/webapp/updateScript.js
+++ src/main/webapp/updateScript.js
@@ -1,35 +1,60 @@
-function updateGame() {
 
-    let body = {};
-    let id = document.getElementById("Game_id").value;
+let id;
+let white = "";
+let black = "";
+let result = "";
+let eco = "";
+let notation = "";
 
-    
-    if (document.getElementById("White").value !== ""){
+const setGameId = (number) => id=number.value;
+const setWhite = (whiteText) => white=whiteText.value;
+const setBlack = (blackText) => black=blackText.value;
+const setResult = (resultText) => result=resultText.value;
+const setEco = (ecoText) => eco=ecoText.value;
+const setNotation = (notationText) => notation=notationText.value;
+
+function findGame() {
+    fetchData("GET", null, "games/" + id).then((value) => {
+        sessionStorage.setItem("userData", value);
+        let body = JSON.parse(value);
         body.white = document.getElementById("White").value;
-    }
-    else {
-        
-        fetchData("GET", null, "games/" + id).then((value) => {
-            info = JSON.parse(value)        
-            document.getElementById("White").value = info.white;
-        }).catch((err) => {
-            console.log('rejected. reason:', err)});
-        console.log('after promise');
-        }
-    
-    body.black = document.getElementById("Black").value;
-    body.result = document.getElementById("Result").value;
-    body.eco = document.getElementById("Eco").value;
-    body.notation = document.getElementById("Notation").value;
-
-    let json = JSON.stringify(body);
-
-    fetchData("PUT", json, "games/" + id).then((
-        document.getElementById("updateMessage").innerHTML = "Success!!"))
-        .catch((err) => {
+        body.black = document.getElementById("Black").value;
+        body.result = document.getElementById("Result").value;
+        body.eco = document.getElementById("Eco").value;
+        body.notation = document.getElementById("Notation").value;
+    }).catch((err) => {
         console.log('rejected. reason:', err)
     });
     console.log('after promise');
-    console.log("record updated");
-
+    
 }
+
+function updateGame() {
+    let body = JSON.parse(sessionStorage.getItem("userData"));
+    if (white !== "") {
+        body.white = white
+    }
+    if ( black !== "") {
+        body.black = black
+    }
+    if ( result !== "") {
+        body.result = result
+    }
+    if ( eco !== "") {
+        body.eco = eco
+    }
+    if ( notation !== "") {
+        body.notation = notation
+    }
+
+        let json = JSON.stringify(body);
+
+        fetchData("PUT", json, "games/" + id).then((
+            document.getElementById("updateMessage").innerHTML = "Success!!"))
+            .catch((err) => {
+                console.log('rejected. reason:', err)
+            });
+        console.log('after promise');
+        console.log("record updated");
+
+    }
